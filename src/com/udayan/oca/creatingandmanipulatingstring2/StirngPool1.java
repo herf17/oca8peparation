a43e22f00b44bd9d46e72f950aa998a7097abc68
herf17 <81944046+herf17@users.noreply.github.com>
2023-09-17 17:24:27 -0500

    Actualizar StirngPool1.java
    subida final errores

--- src/com/udayan/oca/creatingandmanipulatingstring2/StirngPool1.java
+++ src/com/udayan/oca/creatingandmanipulatingstring2/StirngPool1.java
@@ -1,4 +1,12 @@
 package com.udayan.oca.creatingandmanipulatingstring2;
 
-public class StirngPool1 {
+public class StirngPool1{
+    public static void main(String args[]) {
+      String javaworld = "JavaWorld";
+      final String java = "Java";
+      final String world = "World";
+        //java += world;
+        String str1=java+world;
+        System.out.println(str1 == javaworld);
+    }
 }
