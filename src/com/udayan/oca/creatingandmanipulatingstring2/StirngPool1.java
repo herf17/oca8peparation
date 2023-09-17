aa6d2b5139f7ca7f53fb43ea2d1a301296fe62ea
vyper <hiram.rivas17@gmail.com>
2023-09-17 17:36:23 -0500

    pregunta original con respuesta

--- src/com/udayan/oca/creatingandmanipulatingstring2/StirngPool1.java
+++ src/com/udayan/oca/creatingandmanipulatingstring2/StirngPool1.java
@@ -1,12 +1,18 @@
 package com.udayan.oca.creatingandmanipulatingstring2;
-
+/*Validacion de string pool final y  cuando no*/
 public class StirngPool1{
     public static void main(String args[]) {
-      String javaworld = "JavaWorld";
-      final String java = "Java";
-      final String world = "World";
-        //java += world;
-        String str1=java+world;
-        System.out.println(str1 == javaworld);
+        String javaworld = "JavaWorld";
+        String java = "Java";
+        String world = "World";
+        java += world;
+        System.out.println(java == javaworld);
     }
+    /* Answer
+       false
+    * Explanation
+    Please note that Strings computed by concatenation at compile time, will be referred by String Pool during execution. Compile time String concatenation happens when both of the operands are compile time constants, such as literal, final variable etc.
+    Whereas, Strings computed by concatenation at run time (if the resultant expression is not constant expression) are newly created and therefore distinct.
+`   java += world;` is same as `java = java + world;` and `java + world` is not a constant expression and hence is calculated at runtime and returns a non pool String object "JavaWorld", which is referred by variable 'java'.
+    On the other hand, variable 'javaworld' refers to String Pool object "JavaWorld". As both the variables 'java' and 'javaworld' refer to different String objects, hence `java == javaworld` returns false.*/
 }
